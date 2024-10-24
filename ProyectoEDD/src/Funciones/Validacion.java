2f400e2ff5a1f5a85e8cff3e35ba576f65840da1
gabovizcarrondo <vizcarrondo.gabriel@correo.unimet.edu.ve>
2024-10-23 20:46:49 -0400

    crear clase validacion

---
+++ ProyectoEDD/src/Funciones/Validacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Funciones;
+
+/**
+ *
+ * @author vizca
+ */
+public class Validacion {
+    private boolean validarnumeros(String num){
+        return num.matches("[0-9]*");
+    }
+    
+}
