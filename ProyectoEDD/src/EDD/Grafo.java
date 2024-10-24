8946bb0803b590a681263cc424b23adfce40a647
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2024-10-24 16:32:42 -0400

    Fucnion deleteconnection

--- ProyectoEDD/src/EDD/Grafo.java
+++ ProyectoEDD/src/EDD/Grafo.java
@@ -78,4 +78,29 @@ public class Grafo {
         
     }
     }
+    
+    public void deleteConnection(String nombreEstacion1, String nombreEstacion2){
+        if(this.search(nombreEstacion1) != null && this.search(nombreEstacion2) != null){
+            Estacion estacionInicio = search(nombreEstacion1);
+            Estacion estacionFinal = search(nombreEstacion2);
+            
+            if(estacionInicio.getAdyacentes().search(estacionFinal)){
+                estacionInicio.getAdyacentes().deleteForReference(estacionFinal);
+            }else{ 
+                JOptionPane.showMessageDialog(null, "No son adyacentes.");
+            }
+            }
+            estacionInicio.getAdyacentes().insertFinale(estacionFinal);
+            estacionFinal.getAdyacentes().insertFinale(estacionInicio);
+            
+            JOptionPane.showMessageDialog(null, "Conexion agregada con exito.");
+    }else{
+            if(this.search(nombreEstacion1) == null && this.search(nombreEstacion2) != null){
+                JOptionPane.showMessageDialog(null, "La estacion de inicio no existe.");
+            }else if(this.search(nombreEstacion1) != null && this.search(nombreEstacion2) == null){
+                JOptionPane.showMessageDialog(null, "La estacion de llegada no existe.");
+            }else{
+                JOptionPane.showMessageDialog(null, "Ninguna de las estaciones existe.");
+            }
+    }
 }
