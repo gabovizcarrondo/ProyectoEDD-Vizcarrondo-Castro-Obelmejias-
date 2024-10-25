b88df33774596a7439c92f17e9d8fc10de129f0b
samanthacastrorajbe <samanthacastrorajbe@gmail.com>
2024-10-25 01:18:08 -0400

    boton eliminar sucursal funciona

--- ProyectoEDD/src/Interfaces/Sucursal.java
+++ ProyectoEDD/src/Interfaces/Sucursal.java
@@ -89,7 +89,7 @@ public class Sucursal extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox<>();
+        estacionesConSucursal = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         eliminarSucursal = new javax.swing.JButton();
@@ -162,10 +162,10 @@ public class Sucursal extends javax.swing.JFrame {
         jLabel6.setText("-------------------------------------------------------------------------------------------------------------------");
         jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 230, -1, -1));
 
-        jComboBox2.setBackground(new java.awt.Color(204, 204, 255));
-        jComboBox2.setFont(new java.awt.Font("Palatino", 0, 13)); // NOI18N
-        jComboBox2.setModel(modeloConSucursal);
-        jPanel1.add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 320, 270, -1));
+        estacionesConSucursal.setBackground(new java.awt.Color(204, 204, 255));
+        estacionesConSucursal.setFont(new java.awt.Font("Palatino", 0, 13)); // NOI18N
+        estacionesConSucursal.setModel(modeloConSucursal);
+        jPanel1.add(estacionesConSucursal, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 320, 270, -1));
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Interfaces/sucursalnuevo.png"))); // NOI18N
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(-170, 240, -1, 220));
@@ -216,7 +216,11 @@ public class Sucursal extends javax.swing.JFrame {
     }//GEN-LAST:event_aggSucursalActionPerformed
 
     private void eliminarSucursalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_eliminarSucursalActionPerformed
-        // TODO add your handling code here:
+        String nombreEstacion = (String) estacionesConSucursal.getSelectedItem();
+        func.eliminarSucursal(redApp, nombreEstacion);
+        
+        this.llenarComboBoxSinSucursal();
+        this.llenarComboBoxConSucursal();
     }//GEN-LAST:event_eliminarSucursalActionPerformed
 
     /**
@@ -257,9 +261,9 @@ public class Sucursal extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton aggSucursal;
     private javax.swing.JButton eliminarSucursal;
+    private javax.swing.JComboBox<String> estacionesConSucursal;
     private javax.swing.JComboBox<String> estacionesSinSucursal;
     private javax.swing.JToggleButton inicioExit1;
-    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
