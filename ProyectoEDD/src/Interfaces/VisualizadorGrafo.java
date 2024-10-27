b81dd69548574a88496004c7f96b5aeb353c5d3e
gabovizcarrondo <vizcarrondo.gabriel@correo.unimet.edu.ve>
2024-10-27 15:16:13 -0400

    Nueva Funcion inicializarVisor

--- ProyectoEDD/src/Interfaces/VisualizadorGrafo.java
+++ ProyectoEDD/src/Interfaces/VisualizadorGrafo.java
@@ -40,6 +40,20 @@ public class VisualizadorGrafo extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
     }
+    private void inicializarVisor() {
+        Graph grafoVisual = new SingleGraph("Estaciones");
+        construirGrafo(grafoVisual);
+
+        // Mostrar el grafo sin crear una nueva ventana
+        visor = grafoVisual.display(false);
+        visor.enableAutoLayout();  // Permitir que el layout se ajuste automáticamente
+
+        // Creamos el panel de visualización si no existe ya
+        if (panelVista == null) {
+            panelVista = (ViewPanel) visor.getDefaultView();  // Sin ventana adicional
+            add(panelVista, BorderLayout.CENTER);  // Añadir el panel al JFrame
+        }
+    }
 
 
 }
